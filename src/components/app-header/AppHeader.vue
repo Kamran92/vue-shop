77a6d49622257550eb9120d27779f01d06826286
Kamran92 <k.r.a.92@mail.ru>
2022-12-30 16:54:01 +0300

    feat: добавил AppHeader.vue

---
+++ src/components/app-header/AppHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="mb-35 header">
+    <div class="container">
+      <button class="header__btn" @click="toggleOpenLocalities">
+        <location-icon class="header__icon" />
+        <span class="header__title"> Новосибирск </span>
+      </button>
+    </div>
+    <app-cities v-if="isOpenLocalities" @close="toggleOpenLocalities" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import AppCities from "@/components/app-cities/AppCities.vue";
+import locationIcon from "./icon/locationIcon.vue";
+import { ref } from "vue";
+
+const isOpenLocalities = ref<boolean>(false);
+
+const toggleOpenLocalities = (): void => {
+  isOpenLocalities.value = !isOpenLocalities.value;
+};
+</script>
+
+<style scoped>
+.header {
+  padding: 25px 0px;
+  background: #ffffff;
+  box-shadow: 0px 2px 4px rgba(39, 39, 39, 0.1);
+}
+
+.header__btn {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+
+.header__icon {
+  margin-right: 8px;
+  fill: #dadada;
+}
+
+.header__title {
+  font-size: 15px;
+  font-weight: 500;
+  line-height: 24px;
+  letter-spacing: 0px;
+  color: #272727;
+}
+
+.mb-35 {
+  margin-bottom: 35px;
+}
+</style>
