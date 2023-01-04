d26a647d08ee19daaffbbd41913a16a3c720ae44
Kamran92 <k.r.a.92@mail.ru>
2023-01-04 15:36:43 +0300

    feat: добавил AppContainer.vue

---
+++ src/components/AppContainer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{ isLoading: boolean; isError: boolean }>();
+</script>
+
+<template>
+  <div class="container">
+    <div v-if="isLoading">Загрузка...</div>
+    <div v-else-if="isError">Произошла ошибка...</div>
+    <template v-else>
+      <slot></slot>
+    </template>
+  </div>
+</template>
