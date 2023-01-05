6d0015d167b483b3349692bbc18a4ef06b5f53e6
Kamran92 <k.r.a.92@mail.ru>
2023-01-05 09:01:36 +0300

    feat: добавил ProductList.vue

---
+++ src/widget/widget-products/components/ProductList.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import axios from "axios";
+import { storeToRefs } from "pinia";
+import { ref, watch } from "vue";
+import { useRoute } from "vue-router";
+
+import AppContainer from "@/components/AppContainer.vue";
+import useCityIdStore from "@/stores/cityId";
+import ProductCard from "@/widget/widget-products/components/ProductCard.vue";
+
+const { categorySlug } = useRoute().params;
+const props = defineProps<{ activeFilter: string }>();
+
+const { city } = storeToRefs(useCityIdStore());
+
+const products = ref([]);
+const isLoading = ref(false);
+const isError = ref(false);
+
+const getProducts = async () => {
+  try {
+    isLoading.value = true;
+
+    const slug =
+      props.activeFilter === "all" ? categorySlug : props.activeFilter;
+    const URL = `https://nlstar.com/ru/api/catalog3/v1/menutags/${slug}/`;
+
+    const params = { city_id: city.value.id };
+    const { data } = await axios.get(URL, { params });
+    products.value = data.products;
+  } catch (error) {
+    console.error(error);
+    isError.value = true;
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+watch(() => props.activeFilter, getProducts, { immediate: true });
+</script>
+
+<template>
+  <app-container :is-loading="isLoading" :is-error="isError">
+    <ul class="list">
+      <li v-for="(product, index) in products" :key="index" class="list__item">
+        <product-card :product="product" />
+      </li>
+    </ul>
+  </app-container>
+</template>
+
+<style scoped>
+.list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 24px;
+}
+</style>
