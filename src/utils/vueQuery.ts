337b7634625f1a42598aa872d84597c831596a75
Kamran92 <k.r.a.92@mail.ru>
2023-01-05 13:50:07 +0300

    feat: добавил vueQuery.vue

---
+++ src/utils/vueQuery.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const vueQuery = (timeout = 10000) => {
+  interface IRequests {
+    [index: string]: any;
+  }
+  const requests: IRequests = {};
+
+  interface ILoadings {
+    [index: string]: boolean;
+  }
+  const loadings: ILoadings = {};
+
+  return async (link: string) => {
+    if (link in requests) return requests[link];
+    if (link in loadings) return {};
+
+    loadings[link] = true;
+
+    const request = await axios.get(link);
+
+    loadings[link] = false;
+
+    setTimeout(() => {
+      delete requests[link];
+      delete loadings[link];
+    }, timeout);
+
+    requests[link] = request.data;
+
+    return request.data;
+  };
+};
+
+export default vueQuery();
