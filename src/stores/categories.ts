55a9e4d423d804a7f22c796fd31c31fe67152a31
Kamran92 <k.r.a.92@mail.ru>
2023-01-05 09:08:59 +0300

    feat: добавил метод storeGetCategoryBySlug

--- src/stores/categories.ts
+++ src/stores/categories.ts
@@ -7,25 +7,36 @@ import useCityIdStore from "@/stores/cityId";
 export default defineStore("categories", () => {
   const { city } = storeToRefs(useCityIdStore());
 
+  interface IChildren {
+    name: string;
+    slug: string;
+  }
   interface ICategories {
     slug: string;
     image: string;
     text_color: string;
     name: string;
+    children: Array<IChildren>;
   }
   const storeCategories: Ref<Array<ICategories>> = ref([]);
 
   const storeGetCategories = async () => {
-    const URL = "https://nlstar.com/ru/api/catalog3/v1/menutags/";
-    const params = { city_id: city.value.id };
-    const { data } = await axios.get<{ tags: ICategories[] }>(URL, { params });
-    storeCategories.value = data.tags;
+    try {
+      const URL = "https://nlstar.com/ru/api/catalog3/v1/menutags/";
+      const params = { city_id: city.value.id };
+      const { data } = await axios.get<{ tags: ICategories[] }>(URL, {
+        params,
+      });
+      storeCategories.value = data.tags;
+    } catch (error) {
+      console.log(error);
+      throw new Error();
+    }
   };
 
-  const storeGetCategoryName = (slug: string): string => {
-    const category = storeCategories.value.find((i) => i.slug === slug);
-    return category?.name ?? "";
+  const storeGetCategoryBySlug = (slug: string) => {
+    return storeCategories.value.find((i) => i.slug === slug);
   };
 
-  return { storeCategories, storeGetCategories, storeGetCategoryName };
+  return { storeCategories, storeGetCategories, storeGetCategoryBySlug };
 });
