0ee5b41ff24ced82798d495c9e4e622808a5e20d
Kamran92 <k.r.a.92@mail.ru>
2023-01-05 13:48:00 +0300

    feat: добавил ProductFilter.vue

---
+++ src/widget/widget-products/components/ProductFilter.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+import useCategoriesStore from "@/stores/categories";
+
+const props = defineProps<{ modelValue: string }>();
+const emit = defineEmits<{ (e: "update:modelValue", value: string): void }>();
+
+const FILTER_SLUG_ALL = { name: "Все продукты", slug: "all" };
+
+const activeFilter = computed({
+  get: () => props.modelValue,
+  set: (value) => {
+    emit("update:modelValue", value);
+  },
+});
+
+const getFilters = () => {
+  const { storeGetCategoryBySlug } = useCategoriesStore();
+  const { categorySlug } = useRoute().params;
+  if (typeof categorySlug !== "string") return [];
+  const category = storeGetCategoryBySlug(categorySlug);
+  const children = category?.children;
+  return children ? [FILTER_SLUG_ALL, ...children] : [];
+};
+
+const setActiveFilter = (slug: string) => {
+  activeFilter.value = slug;
+};
+
+const isShowFilters = () => {
+  const numberFilters = 2;
+  return getFilters().length > numberFilters;
+};
+
+if (isShowFilters()) {
+  setActiveFilter(FILTER_SLUG_ALL.slug);
+}
+</script>
+
+<template>
+  <ul v-if="isShowFilters()" class="list">
+    <li
+      v-for="({ name, slug }, index) in getFilters()"
+      :key="index"
+      :class="{ 'list__item--active': activeFilter === slug }"
+      class="list__item"
+      @click="setActiveFilter(slug)"
+    >
+      {{ name }}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.list {
+  margin-right: 34px;
+}
+
+.list__item {
+  width: 240px;
+  padding: 8px 12px;
+
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 24px;
+
+  border-bottom: 1px solid #e9eef3;
+  cursor: pointer;
+}
+
+.list__item--active {
+  background-color: #e9eef3;
+}
+</style>
