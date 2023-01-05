e68b151a223bd61f34d23c55e6cd06d7d9817e06
Kamran92 <k.r.a.92@mail.ru>
2023-01-05 13:48:20 +0300

    feat: добавил WidgetProducts.vue

---
+++ src/widget/widget-products/WidgetProducts.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import ProductFilter from "./components/ProductFilter.vue";
+import ProductList from "./components/ProductList.vue";
+
+const activeFilter = ref("all");
+</script>
+
+<template>
+  <article class="products">
+    <product-filter v-model="activeFilter" class="products__filter" />
+    <product-list class="products__list" :active-filter="activeFilter" />
+  </article>
+</template>
+
+<style scoped>
+.products {
+  display: flex;
+}
+
+@media screen and (max-width: 768px) {
+  .products {
+    flex-direction: column;
+  }
+
+  .products__list {
+    margin-top: 34px;
+  }
+}
+</style>
