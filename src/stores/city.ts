445fa30d90e0e085cf2f6284078ffcd4c3cea264
Kamran92 <k.r.a.92@mail.ru>
2023-01-08 14:14:35 +0300

    refactor: city.ts

--- src/stores/city.ts
+++ src/stores/city.ts
@@ -2,15 +2,17 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export default defineStore("city", () => {
-  const city = ref({ id: 1, title: "Новосибирск" });
+  const localStorageCity = localStorage.getItem("city");
+  const defaultCity = localStorageCity
+    ? JSON.parse(localStorageCity)
+    : { id: 1, title: "Новосибирск" };
+
+  const city = ref(defaultCity);
 
   type TCity = typeof city.value;
   const addCity = (value: TCity) => {
     city.value = value;
   };
-
-  const localStorageCity = localStorage.getItem("city");
-  if (localStorageCity) city.value = JSON.parse(localStorageCity);
 
   return { city, addCity };
 });
