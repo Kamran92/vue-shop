dc1be7b6726d2e559eda3b5dcdd8fda3cfd96192
Kamran92 <k.r.a.92@mail.ru>
2023-01-12 11:54:32 +0300

    refactor: debounce.ts

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -2,8 +2,6 @@ export default <T>(callback: (value: T) => Promise<void>, delay = 1000) => {
   let timeout: number;
   return (value: Parameters<typeof callback>[0]) => {
     clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      callback(value);
-    }, delay);
+    timeout = setTimeout(callback, delay, value);
   };
 };
