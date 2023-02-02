7675a579852aaafdce705ec9ccea96bb87bcbf74
Kamran92 <k.r.a.92@mail.ru>
2023-02-02 16:11:29 +0300

    refactor: AppHeader.spec.ts

--- src/components/app-header/__tests__/AppHeader.spec.ts
+++ src/components/app-header/__tests__/AppHeader.spec.ts
@@ -1,30 +1,25 @@
 import AppHeader from "../AppHeader.vue";
 import AppOverlay from "@/components/AppOverlay.vue";
 import { createTestingPinia } from "@pinia/testing";
-import { shallowMount, VueWrapper } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 
+const beforeEachTest = () => {
+  const city = { storeCity: { id: 1, title: "Москва" } };
+  const pinia = createTestingPinia({ initialState: { city } });
+  return shallowMount(AppHeader, { global: { plugins: [pinia] } });
+};
 describe("AppHeader.vue", () => {
-  let wrapper: VueWrapper | null = null;
-
-  beforeEach(() => {
-    const city = { storeCity: { id: 1, title: "Москва" } };
-    const pinia = createTestingPinia({ initialState: { city } });
-    wrapper = shallowMount(AppHeader, { global: { plugins: [pinia] } });
-  });
-
-  afterEach(() => {
-    if (wrapper) wrapper.unmount();
-  });
-
   it("отображается название города", () => {
-    const headerTitle = wrapper!.get(".header__title");
+    const wrapper = beforeEachTest();
+    const headerTitle = wrapper.get(".header__title");
 
     expect(headerTitle.text()).toBe("Москва");
   });
 
   it("при клике отображается поиск городов", async () => {
-    await wrapper!.get(".header__btn").trigger("click");
+    const wrapper = beforeEachTest();
+    await wrapper.get(".header__btn").trigger("click");
 
-    expect(wrapper!.findComponent(AppOverlay).exists()).toBe(true);
+    expect(wrapper.findComponent(AppOverlay).exists()).toBe(true);
   });
 });
