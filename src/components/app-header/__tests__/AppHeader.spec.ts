4f987a1e83813ed31af9cac4b62f075989a30add
Kamran92 <k.r.a.92@mail.ru>
2023-02-05 14:53:48 +0300

    refactor: AppHeader.spec.ts

--- src/components/app-header/__tests__/AppHeader.spec.ts
+++ src/components/app-header/__tests__/AppHeader.spec.ts
@@ -1,13 +1,17 @@
 import AppHeader from "../AppHeader.vue";
 import AppOverlay from "@/components/AppOverlay.vue";
-import { createTestingPinia } from "@pinia/testing";
 import { shallowMount } from "@vue/test-utils";
+import * as pinia from "pinia";
+
+jest.mock("@/stores/cityStore", () => () => "");
+jest.mock("pinia");
 
 const beforeEachTest = () => {
-  const city = { storeCity: { id: 1, title: "Москва" } };
-  const pinia = createTestingPinia({ initialState: { city } });
-  return shallowMount(AppHeader, { global: { plugins: [pinia] } });
+  const mockStoreToRefs = pinia.storeToRefs as jest.Mock;
+  mockStoreToRefs.mockReturnValue({ storeCity: { id: 1, title: "Москва" } });
+  return shallowMount(AppHeader);
 };
+
 describe("AppHeader.vue", () => {
   it("отображается название города", () => {
     const wrapper = beforeEachTest();
