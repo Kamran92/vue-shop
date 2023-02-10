15a456cb1cf5efe6f403d681f66b2f4bad80afaa
Kamran92 <k.r.a.92@mail.ru>
2023-02-10 14:39:45 +0300

    refactor: WidgetProducts.vue

--- src/widgets/widget-products/WidgetProducts.vue
+++ src/widgets/widget-products/WidgetProducts.vue
@@ -1,15 +1,70 @@
 <script setup lang="ts">
+import * as api from "./api";
 import ProductFilter from "./components/ProductFilter.vue";
 import ProductList from "./components/ProductList.vue";
-import { ref } from "vue";
+import AppRequestContainer from "@/components/app-request-container/AppRequestContainer.vue";
+import useCityStore from "@/stores/city-store/cityStore";
+import { storeToRefs } from "pinia";
+import { reactive, watch } from "vue";
+import { useRoute } from "vue-router";
 
-const activeFilter = ref("all");
+const { params } = useRoute();
+
+interface IData {
+  products: Array<{
+    main_image_thumb_300: string;
+    category: { name: string };
+    present_name: string;
+    comment_name: string;
+    price: number;
+    allowed: boolean;
+    available: boolean;
+  }>;
+  isLoading: boolean;
+  isError: boolean;
+  activeFilter: string;
+}
+const data: IData = reactive({
+  products: [],
+  isLoading: false,
+  isError: false,
+  activeFilter: "all",
+});
+
+const getCategorySlug = (categorySlug: unknown, activeFilter: string) => {
+  if (typeof categorySlug !== "string") return "";
+  return activeFilter === "all" ? categorySlug : activeFilter;
+};
+
+const getProducts = async () => {
+  try {
+    data.isLoading = true;
+    const { storeCity } = storeToRefs(useCityStore());
+    data.products = await api.getProducts(
+      storeCity.value.id,
+      getCategorySlug(params.categorySlug, data.activeFilter)
+    );
+  } catch (error) {
+    console.error(error);
+    data.isError = true;
+  } finally {
+    data.isLoading = false;
+  }
+};
+
+watch(() => data.activeFilter, getProducts, { immediate: true });
 </script>
 
 <template>
   <article class="products">
-    <product-filter v-model="activeFilter" class="products__filter" />
-    <product-list class="products__list" :active-filter="activeFilter" />
+    <product-filter v-model="data.activeFilter" class="products__filter" />
+
+    <app-request-container
+      :is-loading="data.isLoading"
+      :is-error="data.isError"
+    >
+      <product-list class="products__list" :products="data.products" />
+    </app-request-container>
   </article>
 </template>
 
--- src/widgets/widget-products/components/ProductList.vue
+++ src/widgets/widget-products/components/ProductList.vue
@@ -1,74 +1,32 @@
 <script setup lang="ts">
-import * as api from "../api";
 import ProductCard from "./ProductCard.vue";
-import AppRequestContainer from "@/components/app-request-container/AppRequestContainer.vue";
-import useCityStore from "@/stores/city-store/cityStore";
-import { storeToRefs } from "pinia";
-import { reactive, watch } from "vue";
-import { useRoute } from "vue-router";
 
-const props = defineProps<{ activeFilter: string }>();
-const { params } = useRoute();
-
-interface IData {
-  products: Array<{
-    main_image_thumb_300: string;
-    category: { name: string };
-    present_name: string;
-    comment_name: string;
-    price: number;
-    allowed: boolean;
-    available: boolean;
-  }>;
-  isLoading: boolean;
-  isError: boolean;
-}
-const data: IData = reactive({
-  products: [],
-  isLoading: false,
-  isError: false,
-});
-
-const getCategorySlug = (categorySlug: unknown, activeFilter: string) => {
-  if (typeof categorySlug !== "string") return "";
-  return activeFilter === "all" ? categorySlug : activeFilter;
-};
-
-const getProducts = async () => {
-  try {
-    data.isLoading = true;
-    const { storeCity } = storeToRefs(useCityStore());
-    data.products = await api.getProducts(
-      storeCity.value.id,
-      getCategorySlug(params.categorySlug, props.activeFilter)
-    );
-  } catch (error) {
-    console.error(error);
-    data.isError = true;
-  } finally {
-    data.isLoading = false;
-  }
-};
-
-watch(() => props.activeFilter, getProducts, { immediate: true });
+type TProducts = Array<{
+  main_image_thumb_300: string;
+  category: { name: string };
+  present_name: string;
+  comment_name: string;
+  price: number;
+  allowed: boolean;
+  available: boolean;
+}>;
+defineProps<{ products: TProducts }>();
 </script>
 
 <template>
-  <app-request-container :is-loading="data.isLoading" :is-error="data.isError">
-    <ul v-if="data.products.length" class="list">
-      <li v-for="(product, index) in data.products" :key="index">
-        <product-card
-          :link-img="product.main_image_thumb_300"
-          :category-name="product.category?.name"
-          :present-name="product.present_name"
-          :comment-name="product.comment_name"
-          :price="product.price"
-          :is-buy="product.allowed && product.available"
-        />
-      </li>
-    </ul>
-    <p v-else>данных нет</p>
-  </app-request-container>
+  <ul v-if="products.length" class="list">
+    <li v-for="(product, index) in products" :key="index">
+      <product-card
+        :link-img="product.main_image_thumb_300"
+        :category-name="product.category?.name"
+        :present-name="product.present_name"
+        :comment-name="product.comment_name"
+        :price="product.price"
+        :is-buy="product.allowed && product.available"
+      />
+    </li>
+  </ul>
+  <p v-else>данных нет</p>
 </template>
 
 <style scoped>
