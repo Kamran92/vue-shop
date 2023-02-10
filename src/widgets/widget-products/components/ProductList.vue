cf4384127da9b69ed3a05d394756bd47d30f46ab
Kamran92 <k.r.a.92@mail.ru>
2023-02-10 14:06:45 +0300

    refactor: ProductList.vue

--- src/widgets/widget-products/components/ProductList.vue
+++ src/widgets/widget-products/components/ProductList.vue
@@ -1,40 +1,52 @@
 <script setup lang="ts">
 import * as api from "../api";
-import * as types from "../types";
 import ProductCard from "./ProductCard.vue";
 import AppRequestContainer from "@/components/app-request-container/AppRequestContainer.vue";
 import useCityStore from "@/stores/city-store/cityStore";
 import { storeToRefs } from "pinia";
-import { Ref, ref, watch } from "vue";
+import { reactive, watch } from "vue";
 import { useRoute } from "vue-router";
 
 const props = defineProps<{ activeFilter: string }>();
-
 const { params } = useRoute();
-const products: Ref<types.TProducts> = ref([]);
-const isLoading = ref(false);
-const isError = ref(false);
 
-const getCategorySlug = () => {
-  const { categorySlug } = params;
+interface IData {
+  products: Array<{
+    main_image_thumb_300: string;
+    category: { name: string };
+    present_name: string;
+    comment_name: string;
+    price: number;
+    allowed: boolean;
+    available: boolean;
+  }>;
+  isLoading: boolean;
+  isError: boolean;
+}
+const data: IData = reactive({
+  products: [],
+  isLoading: false,
+  isError: false,
+});
+
+const getCategorySlug = (categorySlug: unknown, activeFilter: string) => {
   if (typeof categorySlug !== "string") return "";
-  const { activeFilter } = props;
   return activeFilter === "all" ? categorySlug : activeFilter;
 };
 
 const getProducts = async () => {
   try {
-    isLoading.value = true;
+    data.isLoading = true;
     const { storeCity } = storeToRefs(useCityStore());
-    products.value = await api.getProducts(
+    data.products = await api.getProducts(
       storeCity.value.id,
-      getCategorySlug()
+      getCategorySlug(params.categorySlug, props.activeFilter)
     );
   } catch (error) {
     console.error(error);
-    isError.value = true;
+    data.isError = true;
   } finally {
-    isLoading.value = false;
+    data.isLoading = false;
   }
 };
 
@@ -42,9 +54,9 @@ watch(() => props.activeFilter, getProducts, { immediate: true });
 </script>
 
 <template>
-  <app-request-container :is-loading="isLoading" :is-error="isError">
-    <ul v-if="products.length" class="list">
-      <li v-for="(product, index) in products" :key="index" class="list__item">
+  <app-request-container :is-loading="data.isLoading" :is-error="data.isError">
+    <ul v-if="data.products.length" class="list">
+      <li v-for="(product, index) in data.products" :key="index">
         <product-card
           :link-img="product.main_image_thumb_300"
           :category-name="product.category?.name"
